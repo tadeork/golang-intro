d39d13ef5a6eca82cbc5a7f4b5a291e1b67a2d6e
HESPERIDES\tadeo <tadeoriverosk@gmail.com>
2018-04-22 22:12:44 -0300

    no ternary exp in Go :disappointed:

--- hello_1/main.go
+++ hello_1/main.go
@@ -6,8 +6,11 @@ import (
 )
 
 func main() {
-	if len(os.Args) > 1 {
-		fmt.Println(os.Args[1])
+	// the types are inferred in compiling time
+	args := os.Args
+	// sadly theres no ternery expressions in Go
+	if len(args) > 1 {
+		fmt.Println(args[1])
 	} else {
 		fmt.Println("Hello, I'm Gopher")
 	}
